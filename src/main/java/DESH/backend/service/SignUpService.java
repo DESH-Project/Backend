4e1454f79b9a0786357966d091bab1f07dbed190
ehfql6363 <ehfql6363@naver.com>
2023-03-03 18:08:09 +0900

    FEAT(SignUpService.java) : 회원가입 로직 구현
    Signed-off-by: ehfql6363 <ehfql6363@naver.com>

--- src/main/java/DESH/backend/service/SignUpService.java
+++ src/main/java/DESH/backend/service/SignUpService.java
@@ -1,2 +1,20 @@
-package DESH.backend.service;public class SignUpService {
+package DESH.backend.service;
+
+import DESH.backend.dto.SignUpDTO;
+import DESH.backend.entity.User;
+import DESH.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SignUpService {
+
+    private final UserRepository userRepository;
+
+    public User signUp(SignUpDTO signUpDTO) {
+        return userRepository.save(User.builder()
+                .signUpDTO(signUpDTO)
+                .build());
+    }
 }
