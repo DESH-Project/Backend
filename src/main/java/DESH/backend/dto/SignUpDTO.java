9fa2f7754df6b36ad0abef37448ea81935b6761f
ehfql6363 <ehfql6363@naver.com>
2023-03-03 18:05:39 +0900

    FEAT(SignUpDTO.java) : 회원가입에 필요한 DTO 객체 생성
    Signed-off-by: ehfql6363 <ehfql6363@naver.com>

--- src/main/java/DESH/backend/dto/SignUpDTO.java
+++ src/main/java/DESH/backend/dto/SignUpDTO.java
@@ -1,2 +1,20 @@
-package DESH.backend.dto;public class SignUpDTO {
+package DESH.backend.dto;
+
+import DESH.backend.Job;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpDTO {
+
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private Job job;
 }
