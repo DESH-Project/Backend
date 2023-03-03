3dc479461bce5b87708d7e52f583693c9e7d1f46
eun hyang <ssm04090@gmail.com>
2023-03-03 18:13:41 +0900

    FEAT(SignInController.java) : signIn 컨트롤러 생성

--- src/main/java/DESH/backend/controller/SignInController.java
+++ src/main/java/DESH/backend/controller/SignInController.java
@@ -1,4 +1,19 @@
 package DESH.backend.controller;
 
-public class SigninController {
+import DESH.backend.dto.SignInDTO;
+import DESH.backend.service.SignInService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SignInController {
+
+    private final SignInService signInService;
+    @PostMapping("/signIn")
+    public Long signIn(@RequestBody SignInDTO signInDTO){
+        return signInService.signIn(signInDTO);
+    }
 }
