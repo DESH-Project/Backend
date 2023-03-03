008d93332aad33b40f70850722e7882da6b6a0f2
eun hyang <ssm04090@gmail.com>
2023-03-03 19:29:44 +0900

    FEAT(SignInService.java): signin service 생성

--- src/main/java/DESH/backend/service/SignInService.java
+++ src/main/java/DESH/backend/service/SignInService.java
@@ -1,12 +1,24 @@
 package DESH.backend.service;
 
+import DESH.backend.dto.SignInDTO;
+import DESH.backend.entitiy.User;
 import DESH.backend.repositiry.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
-    private UserRepository userRepository;
+@RequiredArgsConstructor
+public class SignInService {
+    private final UserRepository userRepository;
 
-    
+    public Long signIn(SignInDTO signInDTO) {
+        User oldUser = userRepository.findByEmail(signInDTO.getEmail());
+
+        if (oldUser.getPassword().equals(signInDTO.getPassword())){
+            return oldUser.getId();
+        }
+        return null;
+    }
+
 
 }
