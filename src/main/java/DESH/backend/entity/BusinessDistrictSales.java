65781b6bede6d502150e3b72e71f575d596cd058
ehfql6363 <ehfql6363@naver.com>
2023-03-03 15:39:41 +0900

    FEAT(BusinessDistrictSales.java) : 기본 엔티티 생성
    Signed-off-by: ehfql6363 <ehfql6363@naver.com>

--- src/main/java/DESH/backend/entity/BusinessDistrictSales.java
+++ src/main/java/DESH/backend/entity/BusinessDistrictSales.java
@@ -1,2 +1,18 @@
-package DESH.backend.entity;public class BusinessDistrictSales {
+package DESH.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@RequiredArgsConstructor
+@Getter
+public class BusinessDistrictSales {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    
 }
