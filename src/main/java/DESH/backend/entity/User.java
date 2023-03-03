e1609117c7de3d555949b7ab3b078a2478eca9a7
ehfql6363 <ehfql6363@naver.com>
2023-03-03 18:06:19 +0900

    FEAT(User.java) : 사용자 엔티티 생성
    Signed-off-by: ehfql6363 <ehfql6363@naver.com>

--- src/main/java/DESH/backend/entity/User.java
+++ src/main/java/DESH/backend/entity/User.java
@@ -1,4 +1,41 @@
 package DESH.backend.entity;
 
+import DESH.backend.Job;
+import DESH.backend.dto.SignUpDTO;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String name;
+
+    @Column
+    private Job job;
+
+    @Builder
+    public User(SignUpDTO signUpDTO) {
+        this.email = signUpDTO.getEmail();
+        this.password = signUpDTO.getPassword();
+        this.name = signUpDTO.getName();
+        this.job = signUpDTO.getJob();
+    }
 }
