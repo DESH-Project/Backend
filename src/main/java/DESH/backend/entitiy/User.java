78096a68e4208cb087829a3da0f2512bb98625af
eun hyang <ssm04090@gmail.com>
2023-03-03 18:16:36 +0900

    FEAT(User.java): USER 생성

--- src/main/java/DESH/backend/entitiy/User.java
+++ src/main/java/DESH/backend/entitiy/User.java
@@ -1,4 +1,30 @@
 package DESH.backend.entitiy;
 
+import DESH.backend.Job;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+@Getter
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    @Column
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String name;
+
+    @Column
+    private Job job;
+
 }
