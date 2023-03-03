d4a0a17c3474a22c81329303d891ececfe539721
ehfql6363 <ehfql6363@naver.com>
2023-03-03 18:04:41 +0900

    FEAT(SignUpController.java) : 회원가입 매핑 메서드 생성
    Signed-off-by: ehfql6363 <ehfql6363@naver.com>

--- src/main/java/DESH/backend/controller/SignUpController.java
+++ src/main/java/DESH/backend/controller/SignUpController.java
@@ -1,4 +1,21 @@
 package DESH.backend.controller;
 
+import DESH.backend.dto.SignUpDTO;
+import DESH.backend.entity.User;
+import DESH.backend.service.SignUpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class SignUpController {
+
+    private final SignUpService signUpService;
+
+    @PostMapping("/signUp")
+    public User signUp(@RequestBody SignUpDTO signUpDTO) {
+        return signUpService.signUp(signUpDTO);
+    }
 }
