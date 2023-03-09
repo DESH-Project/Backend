00b4e31852b427b7c0ca5d9c3817441b8826fc00
eun hyang <ssm04090@gmail.com>
2023-03-09 17:19:26 +0900

    FEAT (SignInDTO.java) 디티오 생성

--- src/main/java/DESH/backend/dto/SignInDTO.java
+++ src/main/java/DESH/backend/dto/SignInDTO.java
@@ -1,10 +1,13 @@
 package DESH.backend.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class SignInDTO {
-
+    private String name;
     private String email;
     private String password;
+    private String job;
 }
