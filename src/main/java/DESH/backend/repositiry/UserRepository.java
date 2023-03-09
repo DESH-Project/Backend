c052f0fc64113392b30a5149a36394d2388e43b6
eun hyang <ssm04090@gmail.com>
2023-03-09 17:20:26 +0900

    FEAT (UserRepository.java) 레퍼지토리 생성

--- src/main/java/DESH/backend/repositiry/UserRepository.java
+++ src/main/java/DESH/backend/repositiry/UserRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByEmail(String email);
+    User save(String email,String name, String password, String job);
 }
 
